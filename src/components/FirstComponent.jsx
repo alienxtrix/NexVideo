b1f82a4cee957e8de6ccc0bad7cf85f912522307
Josh Wolf <alienxtrix@gmail.com>
2021-01-25 23:06:01 -0600

    Configuracion profesional de proyectos React

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../assets/styles/app.scss'
+
+const FirstComponent =()=>{
+    return(
+        <h2>Primer Componente</h2>
+    )
+}
+export default FirstComponent;
